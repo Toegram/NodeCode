f5bff08e0a490ed1ab28917be9030286aa934ec7
Tony <TTuretsky@gmail.com>
2019-01-15 20:01:51 -0500

    added error handling based on req

---
+++ NodeCode/controllers/storeController.js
@@ -0,0 +1,13 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = "Tony";
+  // res.cookie("name", "Tony Rulez", { maxAge: 1000 });
+  if (req.name === "Tony") {
+    throw Error("Fuck you, Tony");
+  }
+  next();
+};
+
+exports.homePage = (req, res) => {
+  console.log(req.name);
+  res.render("index");
+};
--- NodeCode/routes/index.js
+++ NodeCode/routes/index.js
@@ -1,34 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const storeController = require("../controllers/storeController.js");
 
-//request/response. Request gets an object in, Response is a method that sends data back to the user
-//NEXT also exists. more info later
-router.get("/", (req, res) => {
-  const tony = { name: "Tony", age: 30, cool: true };
-  //CAN ONLY SEND DATA TO THE BROWSER *ONCE*
-  // res.send("Hey! It works!!!!!!!!");
-  // res.json(tony); //sends json object instead of text
-  // res.send(req.query.name);
-  // res.json(req.query);
-  // res.render("hello"); //renders the 'hello.pug' file to the view
-  res.render("hello", {
-    name: "Tony",
-    dogs: ["Olive", "Betty"],
-    cat: req.query.cat,
-    age: 30,
-    cool: true,
-    title: "Foodaholic"
-  });
-});
-
-router.get("/reverse/:name", (req, res) => {
-  //:name is a way to put variables into a route
-  const reversed = [...req.params.name].reverse().join("");
-  res.send(reversed);
-});
+router.get("/", storeController.myMiddleware, storeController.homePage);
 
 module.exports = router;
 
+//request/response. Request gets an object in, Response is a method that sends data back to the user
+//NEXT also exists. more info later
+// router.get("/", (req, res) => {
+//   const tony = { name: "Tony", age: 30, cool: true };
+//   //CAN ONLY SEND DATA TO THE BROWSER *ONCE*
+//   // res.send("Hey! It works!!!!!!!!");
+//   // res.json(tony); //sends json object instead of text
+//   // res.send(req.query.name);
+//   // res.json(req.query);
+//   // res.render("hello"); //renders the 'hello.pug' file to the view
+//   res.render("hello", {
+//     name: "Tony",
+//     dogs: ["Olive", "Betty"],
+//     cat: req.query.cat,
+//     age: 30,
+//     cool: true,
+//     title: "Foodaholic"
+//   });
+// });
+//
+// router.get("/reverse/:name", (req, res) => {
+//   //:name is a way to put variables into a route
+//   const reversed = [...req.params.name].reverse().join("");
+//   res.send(reversed);
+// });
 //REQ has all the information.
 //RES has all the methods for sending data back
 //req.query to get query paramaters
