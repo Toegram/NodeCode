b515b52b2e6f636bbb38915bf2fad7ab2eff281b
Tony <TTuretsky@gmail.com>
2019-01-24 23:05:24 -0500

    added createStore and addStore in storeController, editStore form in view as mixin, and fixed small problem in schema of Store.js

--- NodeCode/controllers/storeController.js
+++ NodeCode/controllers/storeController.js
@@ -3,6 +3,14 @@ exports.homePage = (req, res) => {
   res.render("index");
 };
 
+exports.addStore = (req, res) => {
+  res.render('editStore', {title: 'Add Store'})
+}
+
+exports.createStore = (req, res) => {
+  res.json(req.body)
+}
+
 // exports.myMiddleware = (req, res, next) => {
 //   req.name = "Tony";
 //   // res.cookie("name", "Tony Rulez", { maxAge: 1000 });
