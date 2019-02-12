ac1aa33a5b0bb91b2853e044279370a4c05b1588
Tony <TTuretsky@gmail.com>
2019-02-12 11:18:42 -0500

    updated storeController to handle async await and error catching. Now saves to MongoDB

--- NodeCode/controllers/storeController.js
+++ NodeCode/controllers/storeController.js
@@ -1,15 +1,21 @@
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
+
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render("index");
 };
 
 exports.addStore = (req, res) => {
-  res.render('editStore', {title: 'Add Store'})
-}
+  res.render("editStore", { title: "Add Store" });
+};
 
-exports.createStore = (req, res) => {
-  res.json(req.body)
-}
+exports.createStore = async (req, res) => {
+  //'async' lets compiler know there will be an await
+  const store = new Store(req.body);
+  await store.save(); //'await' tells compiler to wait until this is finished before moving on
+  res.redirect("/");
+};
 
 // exports.myMiddleware = (req, res, next) => {
 //   req.name = "Tony";
