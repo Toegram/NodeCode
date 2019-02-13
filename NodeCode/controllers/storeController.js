d7ae0796a78c7e13dad114a4c1e090cdb1c31abb
Tony <TTuretsky@gmail.com>
2019-02-13 13:44:51 -0500

    users can now upload a photo with a unique id to restaurant

--- NodeCode/controllers/storeController.js
+++ NodeCode/controllers/storeController.js
@@ -1,5 +1,19 @@
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
+const jimp = require("jimp");
+const uuid = require("uuid");
+const multer = require("multer");
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith("image/");
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "The file type is not supported" }, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
   res.render("index");
@@ -7,6 +21,25 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render("editStore", { title: "Add Store" });
+};
+
+exports.upload = multer(multerOptions).single("photo");
+
+exports.resize = async (req, res, next) => {
+  //check if there is no new file to resize
+  if (!req.file) {
+    next(); //skips to the next phase in Middleware
+    return;
+  }
+
+  const extension = req.file.mimetype.split("/")[1];
+  req.body.photo = `${uuid.v4()}.${extension}`; //unique identifier
+
+  //now the resizing!
+  const photo = await jimp.read(req.file.buffer); //sends the file to jimp
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
 };
 
 exports.createStore = async (req, res) => {
