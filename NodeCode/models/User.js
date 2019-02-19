8d03d5e76ac0391fb19a0e248817e17421cb7bf8
Tony <TTuretsky@gmail.com>
2019-02-19 17:37:17 -0500

    created and updated userSchema in User.js

--- NodeCode/models/User.js
+++ NodeCode/models/User.js
@@ -6,6 +6,23 @@ const validator = require("validator");
 const mongoDBErrorHandler = require("mongoose-mongodb-errors");
 const passwordLocalMongoose = require("password-local-mongoose");
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    vaalidate: [validator.isEmail, "Invalid Email Address"],
+    required: "Please Supply An Email Address"
+  },
+  name: {
+    type: String,
+    required: "Please Supply A Name",
+    trim: true
+  }
+});
+
+userSchema.plugin(passwordLocalMongoose, { usernameField: "email" });
+userSchema.plugin(mongoDBErrorHandler);
 
 module.exports(mongoose.model("User", userSchema));
